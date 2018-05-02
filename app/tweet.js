e8b0c0095c51b54a92a1123c57dd1e20dfbb18fd
lpcruz <lcruz@squarespace.com>
2018-05-01 23:33:27 -0400

    Working prototype that will send a notification if parking is suspended

--- app/tweet.js
+++ app/tweet.js
@@ -1,9 +1,24 @@
 const client = require('./client');
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+ "Friday", "Saturday"];
+const d = new Date();
+const data = 'nycasp'
 
-const  params = {screen_name: 'nycasps'};
-client.post('statuses/update', { status: 'This is a test tweet written by nycasps' + ' #testing'},  function(error, tweet, response){
+client.get(`statuses/user_timeline.json?screen_name=${data}&count=1`, function(error, tweets, response) {
   if(error) throw error;
 
-  console.log('✔  Tweet successfully sent to @nycasps');
-
-});
+  if (tweets[0].text.indexOf('suspended') > 0) {
+    
+    client.post('statuses/update', { 
+      status: `${weekdays[d.getDay()]}, ${monthNames[d.getMonth()]} ${d.getDay()}: Alternate Side Parking rules are suspended #NYCASPS`},  
+      function(error, tweet, response) {
+        if(error) throw error;
+        console.log('Parking is suspended. Tweet successfully sent to @nycasps');
+      });
+    } else {
+      console.log('You have to move the damn car')
+    }
+  });
